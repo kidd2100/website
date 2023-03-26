e8f6f3dfb375ebd6451535242c59ab26f3e2bc65
Eric Cauley <75190836+kidd2100@users.noreply.github.com>
2023-03-25 22:46:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 timer = window.setInterval(changeTitle, 50000);
 
 function changeTitle(){
- document.getElementById("Title").innerHTML = "Website";
+ var changeTitle = document.getElementById("Title");
+ changeTitle.innerHTML = "Website";
  if(timer){
    window.clearInterval(timer);
    timer = undefined;
