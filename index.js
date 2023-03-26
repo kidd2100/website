d7d0123607759126e3018d53adbe52221022a895
Eric Cauley <75190836+kidd2100@users.noreply.github.com>
2023-03-25 22:40:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+"use strict"
+/*
+  @auth Eric Cauley
+*/
 
+timer = window.setInterval(changeTitle, 50000);
+
+function changeTitle(){
+ document.getElementById("Title").innerHTML = "Website";
+ if(timer){
+   window.clearInterval(timer);
+   timer = undefined;
+ }
+}
