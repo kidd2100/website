e1c2363227d6ea5b5e0ebe63c1eddbc4b7e781e1
Eric Cauley <75190836+kidd2100@users.noreply.github.com>
2023-03-27 10:18:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,4 @@
 /*
   @auth Eric Cauley
 */
-var timer;
-timer = window.setInterval(openNewWindow, 50000);
-
-function openNewWindow(){
-  let newWin = window.open("www.espn.com", width = 400, height = 400, toolbar = 0, menubar = 0);
-  netWin.moveTo(300,400);
-  
-}
+ 
